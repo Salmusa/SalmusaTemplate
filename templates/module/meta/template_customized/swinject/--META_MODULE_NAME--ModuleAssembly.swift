5fc41c2a98c1918b8afeec77382533fa533b7e07
Jason Nam <contact@jasonnam.com>
2018-12-19 02:12:09 +0700

    Add Swinject module assembly and factory

---
+++ templates/module/meta/template_customized/swinject/--META_MODULE_NAME--ModuleAssembly.swift
@@ -0,0 +1,22 @@
+--INCLUDE:FileHeader--
+
+--INCLUDE:Import--
+import Swinject
+
+final class --META_MODULE_NAME--ModuleAssembly: Assembly {
+
+    func assemble(container: Container) {
+        container.register(--META_MODULE_NAME--Interactor.self) { _ in .init() }
+        container.register(--META_MODULE_NAME--Presenter.self) { _ in .init() }
+        container.register(--META_MODULE_NAME--Router.self) { _ in .init() }
+        container.register(--META_MODULE_NAME--View.self) { _ in .init() }
+        container.register(--META_MODULE_NAME--Module.self) { resolver in
+            .init(
+                interactor: resolver.resolve(--META_MODULE_NAME--Interactor.self)!,
+                presenter: resolver.resolve(--META_MODULE_NAME--Presenter.self)!,
+                router: resolver.resolve(--META_MODULE_NAME--Router.self)!,
+                view: resolver.resolve(--META_MODULE_NAME--View.self)!
+            )
+        }
+    }
+}
---
+++ templates/module/meta/template_customized/swinject/--META_MODULE_NAME--ModuleFactory.swift
@@ -0,0 +1,13 @@
+--INCLUDE:FileHeader--
+
+--INCLUDE:Import--
+import Swinject
+
+final class --META_MODULE_NAME--ModuleFactory {
+
+    static func create() -> --META_MODULE_NAME--Module {
+        let assembler = Assembler()
+        assembler.apply(assembly: --META_MODULE_NAME--ModuleAssembly())
+        return assembler.resolver.resolve(--META_MODULE_NAME--Module.self)!
+    }
+}
