c9aa4bce116effb045952d350b038f0336c70df3
Jason Nam <contact@jasonnam.com>
2018-12-19 02:22:20 +0700

    Update xcode build scripts

--- scripts/build_xcode_intermediate_template.sh
+++ scripts/build_xcode_intermediate_template.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 TEMPLATE_PATH=$1
 BUILD_TARGET_PATH=$2
-CUSTOMIZABLE_FILE_PATH=$3
-TEMPLATE_SPECIFIC_FILE_PATH=$4
+CUSTOMIZED_TEMPLATE_FILE_PATH=$3
+CUSTOMIZED_INCLUDE_FILE_PATH=$4
+FILEHEADER_FILE_PATH=$5
+MODULE_FILE_PATH=$6
 
 # Create build target path
 mkdir -p "$BUILD_TARGET_PATH"
 
 # Copy template files
-cp -R $TEMPLATE_PATH/* "$BUILD_TARGET_PATH"
+cp -R "$TEMPLATE_PATH"/* "$BUILD_TARGET_PATH"
+if [ "$CUSTOMIZED_TEMPLATE_FILE_PATH" != "" ]
+then
+  cp -R "$CUSTOMIZED_TEMPLATE_FILE_PATH"/* "$BUILD_TARGET_PATH/template"
+fi
 
 # Copy import
-cp "$TEMPLATE_SPECIFIC_FILE_PATH/Import" "$BUILD_TARGET_PATH/include/Import"
+cp "$CUSTOMIZED_INCLUDE_FILE_PATH/Import" "$BUILD_TARGET_PATH/include/Import"
 
 # Copy import UIKit
-cp "$TEMPLATE_SPECIFIC_FILE_PATH/ImportUIKit" "$BUILD_TARGET_PATH/include/ImportUIKit"
+cp "$CUSTOMIZED_INCLUDE_FILE_PATH/ImportUIKit" "$BUILD_TARGET_PATH/include/ImportUIKit"
 
 # Copy file header
-cp "$CUSTOMIZABLE_FILE_PATH/FileHeader" "$BUILD_TARGET_PATH/include/FileHeader"
+cp "$FILEHEADER_FILE_PATH" "$BUILD_TARGET_PATH/include/FileHeader"
 
 # Copy Module.swift file
-cp "$TEMPLATE_SPECIFIC_FILE_PATH/Module.swift" "$BUILD_TARGET_PATH/include/Module/Module.swift"
+cp "$MODULE_FILE_PATH" "$BUILD_TARGET_PATH/include/Module/Module.swift"
--- scripts/build_xcode_templates.sh
+++ scripts/build_xcode_templates.sh
@@ -4,9 +4,17 @@ XCODE_CUSTOMIZABLE_FILE_PATH="templates/module/xcode"
 XCODE_PLAIN_TEMPLATE_SPECIFIC_FILE_PATH="$XCODE_CUSTOMIZABLE_FILE_PATH/plain"
 XCODE_FRAMEWORK_TEMPLATE_SPECIFIC_FILE_PATH="$XCODE_CUSTOMIZABLE_FILE_PATH/framework"
 
+XCODE_CUSTOMIZED_TEMPLATE_FILE_PATH="$INTERMEDIATE_TEMPLATE_PATH/template_customized"
+
+XCODE_CUSTOMIZED_INCLUDE_FILE_PATH="$INTERMEDIATE_TEMPLATE_PATH/include_customized"
+XCODE_PLAIN_CUSTOMIZED_INCLUDE_FILE_PATH="$XCODE_CUSTOMIZED_INCLUDE_FILE_PATH/plain"
+XCODE_FRAMEWORK_CUSTOMIZED_INCLUDE_FILE_PATH="$XCODE_CUSTOMIZED_INCLUDE_FILE_PATH/framework"
+
 XCODE_INTERMEDIATE_TEMPLATE_TARGET_PATH="build/module/intermediate/xcode"
-XCODE_INTERMEDIATE_PLAIN_TEMPLATE_TARGET_PATH="$XCODE_INTERMEDIATE_TEMPLATE_TARGET_PATH/plain"
-XCODE_INTERMEDIATE_FRAMEWORK_TEMPLATE_TARGET_PATH="$XCODE_INTERMEDIATE_TEMPLATE_TARGET_PATH/framework"
+XCODE_INTERMEDIATE_PLAIN_TEMPLATE_TARGET_PATH="$XCODE_INTERMEDIATE_TEMPLATE_TARGET_PATH/plain/bare"
+XCODE_INTERMEDIATE_PLAIN_SWINJECT_TEMPLATE_TARGET_PATH="$XCODE_INTERMEDIATE_TEMPLATE_TARGET_PATH/plain/swinject"
+XCODE_INTERMEDIATE_FRAMEWORK_TEMPLATE_TARGET_PATH="$XCODE_INTERMEDIATE_TEMPLATE_TARGET_PATH/framework/bare"
+XCODE_INTERMEDIATE_FRAMEWORK_SWINJECT_TEMPLATE_TARGET_PATH="$XCODE_INTERMEDIATE_TEMPLATE_TARGET_PATH/framework/swinject"
 XCODE_FINAL_TEMPLATE_TARGET_PATH="build/module/xcode/Module.xctemplate"
 
 # Clean build path
@@ -16,21 +24,47 @@ rm -rf $XCODE_FINAL_TEMPLATE_TARGET_PATH
 # Build intermediate Xcode plain template
 sh scripts/build_xcode_intermediate_template.sh "$INTERMEDIATE_TEMPLATE_PATH" \
                                                 "$XCODE_INTERMEDIATE_PLAIN_TEMPLATE_TARGET_PATH" \
-                                                "$XCODE_CUSTOMIZABLE_FILE_PATH" \
-                                                "$XCODE_PLAIN_TEMPLATE_SPECIFIC_FILE_PATH"
+                                                "" \
+                                                "$XCODE_PLAIN_CUSTOMIZED_INCLUDE_FILE_PATH" \
+                                                "$XCODE_CUSTOMIZABLE_FILE_PATH/FileHeader" \
+                                                "$XCODE_PLAIN_CUSTOMIZED_INCLUDE_FILE_PATH/Module.swift"
+# Build intermediate Xcode plain Swinject template
+sh scripts/build_xcode_intermediate_template.sh "$INTERMEDIATE_TEMPLATE_PATH" \
+                                                "$XCODE_INTERMEDIATE_PLAIN_SWINJECT_TEMPLATE_TARGET_PATH" \
+                                                "$XCODE_CUSTOMIZED_TEMPLATE_FILE_PATH/swinject" \
+                                                "$XCODE_PLAIN_CUSTOMIZED_INCLUDE_FILE_PATH" \
+                                                "$XCODE_CUSTOMIZABLE_FILE_PATH/FileHeader" \
+                                                "$XCODE_PLAIN_CUSTOMIZED_INCLUDE_FILE_PATH/swinject/Module.swift"
 # Build intermediate Xcode framework template
 sh scripts/build_xcode_intermediate_template.sh "$INTERMEDIATE_TEMPLATE_PATH" \
                                                 "$XCODE_INTERMEDIATE_FRAMEWORK_TEMPLATE_TARGET_PATH" \
-                                                "$XCODE_CUSTOMIZABLE_FILE_PATH" \
-                                                "$XCODE_FRAMEWORK_TEMPLATE_SPECIFIC_FILE_PATH"
+                                                "" \
+                                                "$XCODE_FRAMEWORK_CUSTOMIZED_INCLUDE_FILE_PATH" \
+                                                "$XCODE_CUSTOMIZABLE_FILE_PATH/FileHeader" \
+                                                "$XCODE_FRAMEWORK_CUSTOMIZED_INCLUDE_FILE_PATH/Module.swift"
+# Build intermediate Xcode framework Swinject template
+sh scripts/build_xcode_intermediate_template.sh "$INTERMEDIATE_TEMPLATE_PATH" \
+                                                "$XCODE_INTERMEDIATE_FRAMEWORK_SWINJECT_TEMPLATE_TARGET_PATH" \
+                                                "$XCODE_CUSTOMIZED_TEMPLATE_FILE_PATH/swinject" \
+                                                "$XCODE_FRAMEWORK_CUSTOMIZED_INCLUDE_FILE_PATH" \
+                                                "$XCODE_CUSTOMIZABLE_FILE_PATH/FileHeader" \
+                                                "$XCODE_FRAMEWORK_CUSTOMIZED_INCLUDE_FILE_PATH/swinject/Module.swift"
 
 # Build Xcode plain template
 summon/bin flint spark -t "$XCODE_INTERMEDIATE_PLAIN_TEMPLATE_TARGET_PATH" \
-                       -o "$XCODE_FINAL_TEMPLATE_TARGET_PATH/NO" \
+                       -o "$XCODE_FINAL_TEMPLATE_TARGET_PATH/No" \
+                       -i "$XCODE_PLAIN_TEMPLATE_SPECIFIC_FILE_PATH/variables.yml"
+# Build Xcode plain Swinject template
+summon/bin flint spark -t "$XCODE_INTERMEDIATE_PLAIN_SWINJECT_TEMPLATE_TARGET_PATH" \
+                       -o "$XCODE_FINAL_TEMPLATE_TARGET_PATH/Swinject" \
                        -i "$XCODE_PLAIN_TEMPLATE_SPECIFIC_FILE_PATH/variables.yml"
 # Build Xcode framework template
 summon/bin flint spark -t "$XCODE_INTERMEDIATE_FRAMEWORK_TEMPLATE_TARGET_PATH" \
-                       -o "$XCODE_FINAL_TEMPLATE_TARGET_PATH/YES" \
+                       -o "$XCODE_FINAL_TEMPLATE_TARGET_PATH/NoSalmusa" \
+                       -i "$XCODE_FRAMEWORK_TEMPLATE_SPECIFIC_FILE_PATH/variables.yml"
+# Build Xcode framework Swinject template
+summon/bin flint spark -t "$XCODE_INTERMEDIATE_FRAMEWORK_SWINJECT_TEMPLATE_TARGET_PATH" \
+                       -o "$XCODE_FINAL_TEMPLATE_TARGET_PATH/SwinjectSalmusa" \
                        -i "$XCODE_FRAMEWORK_TEMPLATE_SPECIFIC_FILE_PATH/variables.yml"
 
 # Copy TemplateInfo.plist
